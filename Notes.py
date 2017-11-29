396edb6b52401c3852584e451e86941df6d17099
SalvadorVp3 <33762499+SalVp3@users.noreply.github.com>
2017-11-29 10:48:18 -0800

    function updated
    11.29.17

--- Notes.py
+++ Notes.py
@@ -1,30 +1,81 @@
-# this is working
-print("hello world")
+# # this is working
+# print("hello world")
+#
+# # salvador
+#
+# print(5 + 3)
+# print(5 - 5)
+# print(7 * 9)
+# print(7**2 * 9)
+# print(6 / 2)
+# print(3 ** 2)
+#
+# print  # this makes a new line. In 3.6, it looks like: print()
+# print("See if you can figure this out")
+# print(10 % 3)
+#
+# car_name = "sal car"
+# car_type = "ford"
+# car_cylinders = 8
+# car_mpg = 9000.1
+#
+# # InLine printing
+# print("I have a car called %s. It's a %s." % (car_name, car_type))
+#
+# # Asking for input
+# name = input("What is your name? ")  # In Python 3, it is just called input()
+# print("Hello %s." % name)
+#
+# age = input("How Old are you? ")
+# print("%s Wow. Thats Old." % age)
 
-# salvador
+#functions
 
-print(5 + 3)
-print(5 - 5)
-print(7 * 9)
-print(7**2 * 9)
-print(6 / 2)
-print(3 ** 2)
 
-print  # this makes a new line. In 3.6, it looks like: print()
-print("See if you can figure this out")
-print(10 % 3)
+def print_hw():
+    print("hello world")
 
-car_name = "sal car"
-car_type = "ford"
-car_cylinders = 8
-car_mpg = 9000.1
 
-# InLine printing
-print("I have a car called %s. It's a %s." % (car_name, car_type))
+print_hw()
+print_hw()
+print_hw()
 
-# Asking for input
-name = input("What is your name? ")  # In Python 3, it is just called input()
-print("Hello %s." % name)
+def say_hi(name): # name is parameter
+    print("hello %s." % name)
+    print("enjoy your day.")
 
-age = input("How Old are you? ")
-print("%s Wow. Thats Old." % age)
+
+say_hi("John")
+
+
+def print_age(name, age):
+    print("%s is %d years old" % (name,age))
+    age += 1  # age = age + 1
+    print("next year, they will be %d" % age)
+
+
+print_age("john", 15)
+
+def f(x):
+   return x**3 + 4 * x**2 + 7 * x - 4
+
+
+print(f(3))
+print(f(4))
+print(f(5))
+
+
+# if statements
+
+def grade_calc(percentage):
+    if percentage >=90:
+         return "A"
+    elif percentage >= 80:
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    elif percentage >= 50:
+        return "F"
+print(grade_calc(100))
