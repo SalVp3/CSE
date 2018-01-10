1b14486029e6205d1e01e91cb2044b658517fe27
SalvadorV p3 <33762499+SalVp3@users.noreply.github.com>
2018-01-10 10:50:17 -0800

    hi
    j

--- Salvador V- Lucky 7's.py
+++ Salvador V- Lucky 7's.py
@@ -1,5 +1,23 @@
 import random
-dic1 = random.randint(1, 6)
-dic2 = random.randint(1, 6)
+dice1 = random.randint(1, 6)
+print("You rolled a %s " % dice1)
+dice2 = random.randint(1, 6)
+print("and you rolled a %s " % dice2)
 
-print(dic1 + dic2)
+print(dice1 + dice2)
+
+number = 7
+money = 15
+
+while money > 0 and number != dice1 + dice2:
+    money = int(number)
+
+
+
+if(dice1 + dice2) == number:
+    money += 4
+else:
+    money -= 1
+
+
+print("You have %s left" % money)
