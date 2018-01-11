a385e0a67c403e974457d4c560fb3decb2756026
SalvadorV p3 <33762499+SalVp3@users.noreply.github.com>
2018-01-11 10:47:59 -0800

    update 2 .1
    1.11.18

--- Salvador V- Lucky 7's.py
+++ Salvador V- Lucky 7's.py
@@ -9,8 +9,6 @@ print(dice1 + dice2)
 number = 7
 money = 15
 
-while money > 0 and number != dice1 + dice2:
-    money = int(number)
 
 
 
@@ -20,4 +18,4 @@ else:
     money -= 1
 
 
-print("You have %s left" % money)
+print("You have $%s left" % money)
