398d9cf28f7b5cddefe8da9c53e9073f75693ae9
SalvadorV p3 <33762499+SalVp3@users.noreply.github.com>
2018-01-16 10:48:51 -0800

    udate 4.1
    1.16.18

--- Salvador V- Lucky 7's.py
+++ Salvador V- Lucky 7's.py
@@ -1,20 +1,24 @@
 import random
 number = 7
 money = 15
+rounds = 0
+highest_money = money
 
 while money > 0:
-
 
     dice1 = random.randint(1, 6)
     print("You rolled a %s " % dice1)
     dice2 = random.randint(1, 6)
     print("and you rolled a %s " % dice2)
-
-    if(dice1 + dice2) == number:
+    total = dice1 + dice2
+    if total == number:
         money += 4
     else:
         money -= 1
-    print(dice1 + dice2)
-
+    print(total)
     print("You have $%s left" % money)
+
+    rounds += 1
+    if money == 0:
+        print("You've played %s rounds" % rounds)
 
