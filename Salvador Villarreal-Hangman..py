e925088a47e76c382ecbcc5ca9fd71213f4f0775
SalvadorV p3 <33762499+SalVp3@users.noreply.github.com>
2018-01-23 10:47:50 -0800

    2
    1-23-18

--- Salvador Villarreal-Hangman..py
+++ Salvador Villarreal-Hangman..py
@@ -1,13 +1,21 @@
+import random, string
 """
 A general guide for hangman
 1.Make a word bank - 10 items
 2.Pick an random item from the list
-3.Hide the word (use *)
+3.Add a guess to the list of letters guessed
 4.Reveal letters already guessed
 5.Create the win condition
 
 """
+guesses_left = 10
+letters_guessed = []
 
+count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 word_bank = ["Spider man", "Hamburgers", "Pluto", "Space Invaders", "YouTube", "The Claw", "Meme lord", "High school",
              "Einstein", "Football"]
-print()
+random.choice(word_bank)
+string.punctuation = '_'
+str1 = random.choice(word_bank)
+underline = str1
+print(underline)
