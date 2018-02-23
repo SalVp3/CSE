21507bb442ef5a9e3346b571e8e36e30252f5e18
SalvadorV p3 <33762499+SalVp3@users.noreply.github.com>
2018-02-23 09:57:05 -0800

    .3
    2-23-18

--- Salvador Villarreal- Dictonary World Map.py
+++ Salvador Villarreal- Dictonary World Map.py
@@ -1,27 +1,93 @@
 world_map = {
     'RUINS': {
         'NAME': "Ruins",
-        'DESCRIPTION': "There are piles of stone. It looks a ruin from something big.",
+        'DESCRIPTION': "There are piles of stone. There are paths to the north, south, east, and west",
         'PATHS': {
-            'NORTH': '1STFOREST',
+            'NORTH': '1FOREST',
             'SOUTH': 'STONEWALL',
             'EAST': 'SIGN',
-            'WEST': ''
+            'WEST': '1GARDEN'
         }
     },
-    '1STFOREST': {
+    '1FOREST': {
         'NAME': 'Spider forest',
-        'DESCRIPTION': "All you see is trees cover in giant cob webs",
+        'DESCRIPTION': " You've arrived at the spider forest.",
         'PATHS': {
             'NORTH': 'PIT',
-            'EAST': ''
-         }
-    },
-    'SOUTHHOUSE': {
-        'NAME': 'South of House',
-        'DESCRIPTION': "Insert Description here",
-        'PATHS': {
-            'NORTH': 'WESTHOUSE'
+            'EAST': 'GRAVE',
+            'WEST': 'STATUE',
+            'SOUTH': 'RUINS'
         }
-    }
+    },
+    'SIGN': {
+        'NAME': "Stop sign",
+        'DESCRIPTION': "You're near a broken stop sign. You can go north, south, or west",
+        'PATHS': {
+            'NORTH': 'LAGOON',
+            'SOUTH': '2FOREST',
+            'WEST': 'RUINS'
+        }
+    },
+    'PIT': {
+        'NAME': "Deep Pit",
+        'DESCRIPTION': "You find a giant hole in the ground. There are paths to the south and west",
+        'PATHS': {
+            'SOUTH': '1FOREST',
+            'WEST': 'POOL'
+        }
+    },
+    'POOL': {
+        'NAME': "POOL",
+        'DESCRIPTION': "You've came across a pool. there are paths to the east and south",
+        'PATHS': {
+            'EAST': 'PIT',
+            'SOUTH': 'STATUE'
+        }
+    },
+    'STATUE': {
+        'NAME': "Grand Statue",
+        'DESCRIPTION': "You arrive at the grand statue and it holding a sword",
+        'PATHS': {
+            'NORTH': 'POOL',
+            'EAST': '1FOREST'
+        }
+    },
+    'GRAVE': {
+        'NAME': "Grave Yard",
+        'DESCRIPTION': "you enter a misty grave yard, you see a shield. You can only go west.",
+        'PATHS': {
+            'WEST': '1FOREST'
+        }
+    },
+    'LAGOON': {
+        'NAME': "Black lagoon",
+        'DESCRIPTION': "You",
+        'PATHS': {
+        }
+    },
+    '': {
+        'NAME': "",
+        'DESCRIPTION': "",
+        'PATHS': {
+        }
+    },
 }
+
+current_node = world_map['RUINS']
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST']
+
+while True:
+    print(current_node['NAME'])
+    print(current_node['DESCRIPTION'])
+    command = input('>_')
+    if command == 'quit':
+        quit(0)
+    if command in directions:
+        try:
+            name_of_node = current_node['PATHS'][command]
+            current_node = world_map[name_of_node]
+        except KeyError:
+            print("you cannot go this way")
+    else:
+        print('Command not Recognized')
+    print()
