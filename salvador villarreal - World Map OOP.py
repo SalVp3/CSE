b15cfae4879c2761b28bf4d98ef0070c10f2ad4d
SalvadorV p3 <33762499+SalVp3@users.noreply.github.com>
2018-02-26 10:36:12 -0800

    hi
    2-26-18

--- salvador villarreal - World Map OOP.py
+++ salvador villarreal - World Map OOP.py
@@ -1,3 +1,12 @@
 class Room(object):
-    def __init__(self):
-        self
+    def __init__(self, name, north,):
+        self.name = name
+        self.north = north
+
+def move(self, direction):
+    global current_node
+    current_node = globals()[getattr(self, direction)]
+
+
+# initialize rooms
+ruins = Room("Ruins", '1forest', 'stone_wall', 'sign', '1garden', None)
